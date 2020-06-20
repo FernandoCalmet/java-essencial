67c5847dc3d564eb4541228da54d4d558ffc6416
Fernando Calmet <fercalmet@gmail.com>
2020-06-19 23:56:24 -0500

    update packages

---
+++ 03-GenericProgramming/HashSet/sizeItems.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+
+class sizeItems {
+    public static void main(String[] args) {
+        HashSet<String> dni = new HashSet<>();
+        dni.add("12345678");
+        dni.add("12121212");
+        dni.add("87236418");
+        dni.add("82650189");
+        dni.add("83494701");
+
+        System.out.println(dni.size());
+    }
+}
