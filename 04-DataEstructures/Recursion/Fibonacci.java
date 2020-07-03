b1e5c3da33487997fa4b5b09abcea05f7d3c0a6f
Fernando Calmet <fercalmet@gmail.com>
2020-07-02 20:43:45 -0500

    Create Fibonacci.java

---
+++ 04-DataEstructures/Recursion/Fibonacci.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar un numero"));
+        JOptionPane.showMessageDialog(null, "Fibonacci recursivo de " + n + " es " + fibonnaciRecursivo(n));
+        JOptionPane.showMessageDialog(null, "Fibonacci por ciclo de " + n + " es " + fibonnaciCiclo(n));
+    }
+
+    public static int fibonnaciRecursivo(int n) {
+        if (n == 1 || n == 2) {
+            // Caso base, respuesta explicita
+            return 1;
+        } else {
+            // Dominio, problema -1
+            return fibonnaciRecursivo(n - 1) + fibonnaciRecursivo(n - 2);
+        }
+    }
+
+    public static int fibonnaciCiclo(int n) {
+        int temp = 0, first = 1, second = 0;
+        while (n > 0) {
+            temp = first + second;
+            first = second;
+            second = temp;
+            n--;
+        }
+        return temp;
+    }
+}
