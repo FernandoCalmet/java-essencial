e2f99e96496c60dd1d13926b5d7d7d3ddf13f6c7
Fernando Calmet <fercalmet@gmail.com>
2020-07-02 19:29:10 -0500

    Update Factorial.java

--- 04-DataEstructures/Recursion/Factorial.java
+++ 04-DataEstructures/Recursion/Factorial.java
@@ -1,15 +1,38 @@
+import javax.swing.JOptionPane;
+
 public class Factorial {
     public static void main(String[] args) {
-        System.out.println(factorial(5));
+        int n = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresar un numero"));
+        JOptionPane.showMessageDialog(null, "Factorial por metodo: !" + n + " = " + factorialMetodo(n));
+        JOptionPane.showMessageDialog(null, "Factorial por ciclo: !" + n + " = " + factorialCiclo(n));
     }
 
-    public static int factorial(int n) {
-        if (n == 0) {
-            // Caso Base: Respuesta explicita
-            return 1;
+    /* Factorial mediante metodo */
+    public static int factorialMetodo(int n) {
+        if (n < 0) {
+            return 0;
         } else {
-            // Dominio: Problema -1
-            return n * factorial(n - 1);
+            if (n == 0) {
+                // Caso Base: Respuesta explicita
+                return 1;
+            } else {
+                // Dominio: Problema -1
+                return n * factorialMetodo(n - 1);
+            }
+        }
+    }
+
+    /* Factorial mediante ciclo */
+    public static int factorialCiclo(int n) {
+        int factor = 1;
+        if (n < 0) {
+            return 0;
+        } else {
+            while (n != 0) {
+                factor = n * factor;
+                n--;
+            }
+            return factor;
         }
     }
 }
