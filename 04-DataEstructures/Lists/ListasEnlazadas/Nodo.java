b33d6a52173c09f799ee80d4a80d01e37b7e652a
Fernando Calmet <fercalmet@gmail.com>
2020-07-03 22:07:12 -0500

    lista simplemente enlazada

---
+++ 04-DataEstructures/Lists/ListasEnlazadas/Nodo.java
@@ -0,0 +1,17 @@
+package ListasEnlazadas;
+
+public class Nodo {
+    public int dato;
+    public Nodo siguiente; // Puntero
+
+    // Para insertar al final
+    public Nodo(int dato) {
+        this.dato = dato;
+    }
+
+    // Para insertar al inicio
+    public Nodo(int dato, Nodo nodo) {
+        this.dato = dato;
+        this.siguiente = nodo;
+    }
+}
