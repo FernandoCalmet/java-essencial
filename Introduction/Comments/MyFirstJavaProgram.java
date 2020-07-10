fb72e0cda5f34532fab88e003a84b0dfab79ab40
Fernando Calmet <fercalmet@gmail.com>
2020-07-09 22:23:01 -0500

    update docs and packages

---
+++ Introduction/Comments/MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+package Comments;
+
+public class MyFirstJavaProgram {
+
+    /*
+     * This is my first java program. This will print 'Hello World' as the output
+     * This is an example of multi-line comments.
+     */
+
+    public static void main(String[] args) {
+        // This is an example of single line comment
+        /* This is also an example of single line comment. */
+        System.out.println("Hello World");
+    }
+}
---
+++ Introduction/Enum/FreshJuice.java
@@ -0,0 +1,18 @@
+package Enum;
+
+class FreshJuice {
+    enum FreshJuiceSize {
+        SMALL, MEDIUM, LARGE
+    }
+
+    FreshJuiceSize size;
+}
+
+class FreshJuiceTest {
+
+    public static void main(String args[]) {
+        FreshJuice juice = new FreshJuice();
+        juice.size = FreshJuice.FreshJuiceSize.MEDIUM;
+        System.out.println("Size: " + juice.size);
+    }
+}
